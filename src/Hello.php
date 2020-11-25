487ef51e4a760d63247ec0bfa3a663d679ea03e8
Alexey Manko <alexey.manko@tpg.ua>
2020-11-25 18:45:55 +0200

    25112020-00

---
+++ src/Hello.php
@@ -0,0 +1,21 @@
+<?php
+namespace manko\src;
+
+/*
+ echo "# composer-pack" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/cln2008/composer-pack.git
+git push -u origin main
+ */
+
+class Hello
+{
+
+    public function getHelo()
+    {
+        return "Hello world !";
+    }
+}
